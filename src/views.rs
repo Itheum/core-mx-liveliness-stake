b9703505a1e0b706de549fbc446559d203f7d637
Bucur David <dbucur22@gmail.com>
2024-07-04 15:07:41 +0300

    refactor: view can bypass liveliness score

--- src/views.rs
+++ src/views.rs
@@ -8,13 +8,17 @@ pub trait ViewsModule:
     rewards::RewardsModule + events::EventsModule + config::ConfigModule + storage::StorageModule
 {
     #[view(claimableRewards)]
-    fn claimable_rewards(&self, caller: ManagedAddress, opt_timestamp: Option<u64>) -> BigUint {
-        let timestamp = opt_timestamp.unwrap_or(self.blockchain().get_block_timestamp());
+    fn claimable_rewards(
+        &self,
+        caller: ManagedAddress,
+        opt_bypass_liveliness: Option<bool>,
+    ) -> BigUint {
+        let bypass_liveliness = opt_bypass_liveliness.unwrap_or(false);
 
         let mut storage_cache = StorageCache::new(self);
 
         self.generate_aggregated_rewards(&mut storage_cache);
 
-        self.calculate_caller_share_in_rewards(&caller, &mut storage_cache, Some(timestamp))
+        self.calculate_caller_share_in_rewards(&caller, &mut storage_cache, bypass_liveliness)
     }
 }
