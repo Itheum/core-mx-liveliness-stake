3bfb63fa8786662b0a48931ddaf361f57bf7f839
Bucur David <dbucur22@gmail.com>
2024-07-06 17:54:13 +0300

    feat: admin snippets

---
+++ interaction/devnet.snippets.sh
@@ -0,0 +1,201 @@
+PROXY=https://devnet-gateway.multiversx.com
+CHAIN_ID="D"
+
+WALLET="../wallet.pem"
+USER="../wallet2.pem"
+
+
+ADDRESS=$(mxpy data load --key=address-devnet)
+DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-devnet)
+
+TOKEN="ITHEUM-fce905"
+TOKEN_HEX="0x$(echo -n ${TOKEN} | xxd -p -u | tr -d '\n')"
+
+# to deploy from last reprodubible build, we need to change or vice versa
+# --bytecode output/core-mx-life-bonding-sc.wasm \
+# to 
+# --bytecode output-docker/core-mx-life-bonding-sc/core-mx-life-bonding-sc.wasm \
+deploy(){
+    mxpy --verbose contract deploy \
+    --bytecode output/core-mx-liveliness-stake.wasm\
+    --outfile deployOutput \
+    --metadata-not-readable \
+    --metadata-payable-by-sc \
+    --pem ${WALLET} \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --gas-limit 150000000 \
+    --send \
+    --recall-nonce \
+    --outfile="./interaction/deploy-devnet.interaction.json" || return
+
+    TRANSACTION=$(mxpy data parse --file="./interaction/deploy-devnet.interaction.json" --expression="data['emittedTransactionHash']")
+    ADDRESS=$(mxpy data parse --file="./interaction/deploy-devnet.interaction.json" --expression="data['contractAddress']")
+
+    mxpy data store --key=address-devnet --value=${ADDRESS}
+    mxpy data store --key=deployTransaction-devnet --value=${TRANSACTION}
+}
+
+# any change to code or property requires a full upgrade 
+# always check if you are deploy via a reprodubible build and that the code hash is the same before and after upgrade (that is if you are only changing props and not code.. for code, the RB will be different)
+# if only changing props, you can't just "append" new props. you have to add the old ones again and then add a new prop you need. i.e. it's not append, it's a whole reset
+# for upgrade, --outfile deployOutput is not needed
+# in below code example we added --metadata-payable to add PAYABLE to the prop of the SC and removed --metadata-not-readable to make it READABLE
+upgrade(){
+    mxpy --verbose contract upgrade ${ADDRESS} \
+    --bytecode output-docker/core-mx-life-bonding-sc/core-mx-life-bonding-sc.wasm \
+    --metadata-not-readable \
+    --metadata-payable-by-sc \
+    --pem ${WALLET} \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --gas-limit 150000000 \
+    --recall-nonce \
+    --send || return
+}
+
+setAdministrator(){
+    # $1 = address
+
+    address="0x$(mxpy wallet bech32 --decode ${1})"
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setAdministrator" \
+    --arguments $address \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+
+setContractStateActive(){
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setContractStateActive" \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+setContractStateInactive(){
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setContractStateInactive" \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+
+setRewardsTokenIdentifier(){
+    # $1 = token identifier
+
+    token="0x$(echo -n ${1} | xxd -p -u | tr -d '\n')"
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setRewardsTokenIdentifier" \
+    --arguments $token \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+
+}
+
+setPerBlockRewardAmount(){
+    # $1 = amount (with token decimals)
+
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setPerBlockRewardAmount" \
+    --arguments $1 \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+topUpRewards(){
+    # $1 = amount of esdt to send 
+
+    method="0x$(echo -n "topUpRewards" | xxd -p -u | tr -d '\n')"
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "ESDTTransfer" \
+    --arguments ${TOKEN_HEX} $1 $method \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+withdrawRewards(){
+    # $1 = amount of esdt to receive
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "withdrawRewards" \
+    --arguments $1 \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+
+}
+
+
+startProduceRewards(){
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "startProduceRewards" \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+endProduceRewards(){
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "endProduceRewards" \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
+
+
+setBondContractAddress(){
+    # $1 = address
+
+    address="0x$(mxpy wallet bech32 --decode ${1})"
+
+    mxpy --verbose contract call ${ADDRESS} \
+    --recall-nonce \
+    --pem=${WALLET} \
+    --gas-limit=6000000 \
+    --function "setBondContractAddress" \
+    --arguments $address \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --send || return
+}
